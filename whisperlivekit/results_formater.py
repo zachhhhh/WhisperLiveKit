4ed62e181daa7465751962cd0ebaa899f2b6da06
Quentin Fuxa <quentin.fuxa@gmail.com>
2024-08-24 19:24:00 +0200

    when silences are detected, speaker correction is no more applied

--- whisperlivekit/results_formater.py
+++ whisperlivekit/results_formater.py
@@ -119,15 +119,15 @@ def format_output(state, silence, current_time, diarization, debug):
         
 
         if speaker != previous_speaker:
-            if next_punctuation_change(i, tokens):
+            if speaker == -2 or previous_speaker == -2: #silences can happen anytime
+                lines.append(new_line(token, speaker, last_end_diarized, debug_info = ""))
+                continue
+            elif next_punctuation_change(i, tokens):
                 # Corrects advance:
                 # Are you |SPLIT SPEAKER| okay? yeah, sure. Absolutely 
                 # should become:
                 # Are you okay? |SPLIT SPEAKER| yeah, sure. Absolutely 
                 append_token_to_last_line(lines, sep, token, debug_info, last_end_diarized)
-                continue
-            elif speaker == -2 or previous_speaker == -2: #silences can happen anytime
-                lines.append(new_line(token, speaker, last_end_diarized, debug_info = ""))
                 continue
             else: #we create a new speaker, but that's no ideal. We are not sure about the split. We prefer to append to previous line
                 # lines.append(new_line(token, speaker, last_end_diarized, debug_info = ""))
